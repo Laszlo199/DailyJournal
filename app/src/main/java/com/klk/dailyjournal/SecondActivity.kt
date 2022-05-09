fe59f752350d887dbaf9a930dbca78557c4c1474
kubaczechowski <67064580+kubaczechowski@users.noreply.github.com>
2022-05-10 01:10:12 +0200

    added functionality for adding a photo from gallery

--- app/src/main/java/com/klk/dailyjournal/SecondActivity.kt
+++ app/src/main/java/com/klk/dailyjournal/SecondActivity.kt
@@ -1,6 +1,7 @@
 package com.klk.dailyjournal
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
@@ -18,26 +19,107 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
+import com.klk.dailyjournal.data.NoteEntity
+import com.klk.dailyjournal.data.NoteRepository
 import com.klk.dailyjournal.entities.Feeling
 import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class SecondActivity: AppCompatActivity(){
-    var gratefulFor: String = ""
-    var highlightOfTheDay: String = ""
-    var personalNote: String = ""
+    val calendar = Calendar.getInstance()
+
     var photo: File? = null
+    var pathPhoto: String? = null
     var  mood: Feeling?= null
+    val repo = NoteRepository.get()
+
 
     val TAG = "xyz"
-    private val PERMISSION_REQUEST_CODE = 1
+    private val TAKE_PHOTO_PERMISSION_REQUEST_CODE = 1
 
 
     fun saveNote(view: View){
         val gratefulnessEdit = findViewById<TextView>(R.id.gratefulness_edit)
         val todayEdit = findViewById<TextView>(R.id.today_edit)
         val myNoteEdit = findViewById<TextView>(R.id.my_note_edit)
+
+        val path  = if (pathPhoto==null)  photo?.path else pathPhoto
+        repo.insert(NoteEntity(
+            0,
+            SimpleDateFormat("EEEE", Locale.ENGLISH).
+            format(getDate()),
+            "${getDayAsString()} ${getMonthName()}",
+            "face_temporary.png",
+            gratefulnessEdit.text.toString(),
+            todayEdit.text.toString(),
+            myNoteEdit.text.toString(),
+            path, null))
+
+            //save
+    }
+
+    fun pickImageFromGallery(view: View){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        singleImageResultLauncher.launch(Intent.createChooser(intent, "Select Picture"))
+    }
+
+    val singleImageResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val selectedImageUri: Uri? = data?.data
+                if (null != selectedImageUri) {
+                    findViewById<ImageView>(R.id.take_photo_img).setImageURI(selectedImageUri)
+                    photo = null
+                    pathPhoto = getPathFromURI(selectedImageUri)
+
+                }
+            }
+        }
+
+    private fun getPathFromURI(uri: Uri?): String {
+        var path = ""
+        if (contentResolver != null) {
+            val cursor = contentResolver.query(uri!!, null, null, null, null)
+            if (cursor != null) {
+                cursor.moveToFirst()
+                val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME)
+                path = cursor.getString(idx)
+                cursor.close()
+            }
+        }
+        return path
+    }
+
+
+    private fun getDate(): Date{
+        return calendar.time
+    }
+
+    private fun getMonthName():String{
+        val month_date = SimpleDateFormat("MMMM")
+        return month_date.format(calendar.time)
+    }
+
+    private fun  getDayAsString(): String{
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        var dayStr = ""
+        if(day.toString().endsWith('1'))
+            dayStr = day.toString() +"st"
+        else if(day.toString().endsWith('2'))
+            dayStr = day.toString() +"nd"
+        else if(day.toString().endsWith('3'))
+            dayStr = day.toString() +"rd"
+        else
+            dayStr = day.toString() + "th"
+
+        return dayStr
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,15 +129,21 @@ class SecondActivity: AppCompatActivity(){
         val name = intent.getStringExtra("feeling_passed")
         mood = name?.let { getEnum(it) }
 
+        val date = SimpleDateFormat("EEEE", Locale.ENGLISH).
+        format(getDate()) +", "+ getMonthName() +
+                " "+ getDayAsString()
+        val dateT = findViewById<TextView>(R.id.date)
+        dateT.text = date
+
         checkPermissions()
     }
 
     private fun getEnum(name: String): Feeling{
         for(i in Feeling.values())
             if(name == i.name)
-                return i;
+                return i
 
-        return Feeling.OK;
+        return Feeling.OK
     }
 
 
@@ -63,10 +151,14 @@ class SecondActivity: AppCompatActivity(){
         ActivityResultContracts.StartActivityForResult()
     ){ activityResult ->
         val mImage = findViewById<ImageView>(R.id.take_photo_img)
-        if (activityResult.resultCode == RESULT_OK)
+        if (activityResult.resultCode == RESULT_OK){
+            pathPhoto=null
             showImageFromFile(mImage, photo!!)
+        }
         else handleOther(activityResult.resultCode)
     }
+
+
 
     private fun showImageFromFile(img: ImageView,  f: File) {
         img.setImageURI(Uri.fromFile(f))
@@ -82,7 +174,6 @@ class SecondActivity: AppCompatActivity(){
         }
 
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
         // Add extra to inform the app where to put the image.
         val applicationId = "com.klk.dailyjournal"
         intent.putExtra(
@@ -94,9 +185,6 @@ class SecondActivity: AppCompatActivity(){
         fileCallback.launch(intent)
     }
 
-    // return a new file with a timestamp name in a folder named [folder] in
-    // the external directory for pictures.
-    // Return null if the file cannot be created
     private fun getOutputMediaFile(folder: String): File? {
         // in an emulated device you can see the external files in /sdcard/Android/data/<your app>.
         val mediaStorageDir = File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), folder)
@@ -107,11 +195,11 @@ class SecondActivity: AppCompatActivity(){
                 return null
             }
         }
-
         // Create a media file name
         val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val postfix = "jpg"
         val prefix = "IMG"
+
         return File(mediaStorageDir.path +
                 File.separator + prefix +
                 "_" + timeStamp + "." + postfix)
@@ -124,7 +212,7 @@ class SecondActivity: AppCompatActivity(){
         if ( ! isGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE) ) permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         if ( ! isGranted(Manifest.permission.CAMERA) ) permissions.add(Manifest.permission.CAMERA)
         if (permissions.size > 0)
-            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PERMISSION_REQUEST_CODE)
+            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), TAKE_PHOTO_PERMISSION_REQUEST_CODE)
     }
 
     private fun isGranted(permission: String): Boolean =
@@ -135,4 +223,5 @@ class SecondActivity: AppCompatActivity(){
             Toast.makeText(this, "Canceled...", Toast.LENGTH_LONG).show()
         else Toast.makeText(this, "Picture NOT taken - unknown error...", Toast.LENGTH_LONG).show()
     }
+
 }
--- app/src/main/java/com/klk/dailyjournal/entities/Feeling.kt
+++ app/src/main/java/com/klk/dailyjournal/entities/Feeling.kt
@@ -1,12 +1,9 @@
 package com.klk.dailyjournal.entities
 
 enum class Feeling(val i: Int) {
-
     SAD(1),
     BAD(2),
     OK(3),
     HAPPY(4),
     OVERJOYED(5);
-
-
 }
