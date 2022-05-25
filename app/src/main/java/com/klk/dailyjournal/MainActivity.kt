a9f7847b5035032268e5381da4b01295047cac63
kamila0801 <kami7686@easv365.dk>
2022-05-25 17:14:02 +0200

    fix after merge

--- app/src/main/java/com/klk/dailyjournal/MainActivity.kt
+++ app/src/main/java/com/klk/dailyjournal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
